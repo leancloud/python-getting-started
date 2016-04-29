5ba6565dd0bde9a9cdccee05bc23667750a01644
asaka <aisk1988@gmail.com>
2016-04-29 17:19:41 +0800

    feat: add before save hook demo

--- cloud.py
+++ cloud.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from leancloud import Engine
+from leancloud import LeanEngineError
 
 from app import app
 
@@ -14,3 +15,12 @@ def hello(**params):
         return 'Hello, {}!'.format(params['name'])
     else:
         return 'Hello, LeanCloud!'
+
+
+@engine.before_save('Todo')
+def before_todo_save(todo):
+    content = todo.get('content')
+    if not content:
+        raise LeanEngineError('内容不能为空')
+    if len(content) >= 240:
+        todo.set('content', content[:240] + ' ...')
