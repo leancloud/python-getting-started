1ab2bc5a20ee52e357d8de34e4db00d58cf7289e
Long Ang <aisk1988@gmail.com>
2016-09-28 17:54:12 +0800

    feat: do not use  master key on default

--- wsgi.py
+++ wsgi.py
@@ -13,11 +13,13 @@ from app import app
 from cloud import engine
 
 APP_ID = os.environ['LEANCLOUD_APP_ID']
+APP_KEY = os.environ['LEANCLOUD_APP_KEY']
 MASTER_KEY = os.environ['LEANCLOUD_APP_MASTER_KEY']
 PORT = int(os.environ['LEANCLOUD_APP_PORT'])
 
-
-leancloud.init(APP_ID, master_key=MASTER_KEY)
+leancloud.init(APP_ID, app_key=APP_KEY, master_key=MASTER_KEY)
+# 如果需要使用 master key 权限访问 LeanCLoud 服务，请将这里设置为 True
+leancloud.use_master_key(False)
 
 application = engine
 
