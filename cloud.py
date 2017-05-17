acff3a6cc6c323b08cc5e59922c61357ff7df031
Ang Long <aisk@users.noreply.github.com>
2017-05-17 15:31:59 +0800

    feat: add https redirect middleware codes (#15)

--- cloud.py
+++ cloud.py
@@ -1,11 +1,14 @@
 # coding: utf-8
 
+from leancloud import HttpsRedirectMiddleware
 from leancloud import Engine
 from leancloud import LeanEngineError
 
 from app import app
 
 
+# 需要重定向到 HTTPS 可去除下一行的注释。
+# app = HttpsRedirectMiddleware(app)
 engine = Engine(app)
 
 
